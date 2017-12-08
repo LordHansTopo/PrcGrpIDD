2e2bc1c691a2917a4cef3b57b4c7a2864403b205
ChaosMillenium <darkrai.ivan@gmail.com>
2017-12-08 17:26:56 +0100

    Delete Main.java

--- SimuladorInversiones/General/Main.java
+++
@@ -1,9 +0,0 @@
-package SimuladorInversiones.General;
-
-public class Main {
-
-    public static void main(String[] args) {
-	    Simulador simulador = new Simulador();
-        simulador.comenzarSimulacion();
-    }
-}
