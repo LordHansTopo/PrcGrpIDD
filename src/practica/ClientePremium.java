d97aa45b99ac28cff0afbd47494627d936c64c61
ChaosMillenium <darkrai.ivan@gmail.com>
2017-12-10 16:20:43 +0100

    Delete ClientePremium.java

--- src/practica/ClientePremium.java
+++
@@ -1,9 +0,0 @@
-package practica;
-
-public class ClientePremium extends Cliente {
-
-    public ClientePremium(String nombre, String DNI, Double saldo){
-        super(nombre, DNI, saldo);
-    }
-
-}
