815d9d6e55d95ac821f5d0d0a191e2f89c391207
ChaosMillenium <darkrai.ivan@gmail.com>
2018-01-03 17:13:52 +0100

    Delete ExcepcionNoExisteValor.java

--- Excepciones/ExcepcionNoExisteValor.java
+++
@@ -1,6 +0,0 @@
-package Excepciones;
-
-public class ExcepcionNoExisteValor extends Exception {
-    public ExcepcionNoExisteValor(){super();}
-    public ExcepcionNoExisteValor(String msg){super(msg);}
-}
