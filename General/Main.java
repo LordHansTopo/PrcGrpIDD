3da1133d18fae442221d18cbb31d7aaa21d47126
ChaosMillenium <darkrai.ivan@gmail.com>
2018-01-08 17:55:21 +0100

    Update Main.java

--- General/Main.java
+++ General/Main.java
@@ -1,3 +1,10 @@
+/*
+Simulador de inversiones que simula un banco, una bolsa, los agentes que intervienen en estos y acciones que simulan inversiones.
+Autores:
+Iván Chicano Capelo
+Daniel Diz Molinero
+David Muñoz Alonso
+*/
 package General;
 
 public class Main {
